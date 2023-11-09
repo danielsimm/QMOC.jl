a12693d9862fa4b224053ee4cf35d0b8754f5795
danielsimm <daniel.simm@outlook.com>
2023-11-09 15:13:17 +0100

    test cleanup

--- test/ykxyztest.jl
+++
@@ -1,7 +0,0 @@
-using Distributed
-addprocs(4, exeflags="-t2")
-@everywhere using LatticeCircuits
-
-sim = simulation(:YaoKivelsonXYZ, 36, "YaoKivelsonIsotropic36", 100, [[1//3, 1//3, 1//3]], checkpoints=true)
-
-simulate(sim)
