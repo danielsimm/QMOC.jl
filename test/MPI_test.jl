c11d508f239af1d77e2e09f6e55e66bd36265787
danielsimm <daniel.simm@outlook.com>
2023-11-13 00:26:42 +0100

    lattice plotting tools

--- test/MPI_test.jl
+++ test/MPI_test.jl
@@ -1,2 +1,4 @@
 using QMOC
-simulate(QMOC.test_simulation_large(:Kekule))
+parameter_set = [[1/(1+K), K/(1+K)] for K in 0:0.5:10]
+sim = simulation(:YaoKivelsonNonorientable, 12, "YaoKivelsonNonorientable_12", 200, parameter_set; checkpoints=false, verbosity=:info)
+simulate(sim)
