90be996a20df5b65f4d80f8e406e82549834f37f
danielsimm <daniel.simm@outlook.com>
2023-12-30 18:13:33 +0100

    clipped gauge EE test

--- src/YaoKivelson.jl
+++ src/YaoKivelson.jl
@@ -2,42 +2,42 @@ abstract type DecoratedHoneycombTrajectory <: Trajectory end
 
 abstract type YaoKivelsonJKTrajectory <: DecoratedHoneycombTrajectory end
 struct YaoKivelsonXYZTrajectory <: DecoratedHoneycombTrajectory
-    size::Int
-    nqubits::Int
-    name::String
-    params::Vector{Any}
-    checkpoints::Bool
-    verbosity::Symbol
-    index::Int64
-    thermalization_steps::Int64
-    measurement_steps::Int64
-    number_of_measurements::Int64
+	size::Int
+	nqubits::Int
+	name::String
+	params::Vector{Any}
+	checkpoints::Bool
+	verbosity::Symbol
+	index::Int64
+	thermalization_steps::Int64
+	measurement_steps::Int64
+	number_of_measurements::Int64
 end
 
 struct YaoKivelsonOrientableTrajectory <: YaoKivelsonJKTrajectory
-    size::Int
-    nqubits::Int
-    name::String
-    params::Vector{Any}
-    checkpoints::Bool
-    verbosity::Symbol
-    index::Int64
-    thermalization_steps::Int64
-    measurement_steps::Int64
-    number_of_measurements::Int64
+	size::Int
+	nqubits::Int
+	name::String
+	params::Vector{Any}
+	checkpoints::Bool
+	verbosity::Symbol
+	index::Int64
+	thermalization_steps::Int64
+	measurement_steps::Int64
+	number_of_measurements::Int64
 end
 
 struct YaoKivelsonNonorientableTrajectory <: YaoKivelsonJKTrajectory
-    size::Int
-    nqubits::Int
-    name::String
-    params::Vector{Any}
-    checkpoints::Bool
-    verbosity::Symbol
-    index::Int64
-    thermalization_steps::Int64
-    measurement_steps::Int64
-    number_of_measurements::Int64
+	size::Int
+	nqubits::Int
+	name::String
+	params::Vector{Any}
+	checkpoints::Bool
+	verbosity::Symbol
+	index::Int64
+	thermalization_steps::Int64
+	measurement_steps::Int64
+	number_of_measurements::Int64
 end
 
 export YaoKivelsonXYZTrajectory, YaoKivelsonNonorientableTrajectory, YaoKivelsonOrientableTrajectory, DecoratedHoneycombTrajectory
@@ -47,44 +47,44 @@ export YaoKivelsonXYZTrajectory, YaoKivelsonNonorientableTrajectory, YaoKivelson
 include("YaoKivelsonOperators.jl")
 
 function get_operators(trajectory::YaoKivelsonXYZTrajectory)
-    matrix = Matrix{PauliOperator}(undef, 3, 3*trajectory.size^2)
-    matrix[1, :] = _DHC_XX_operators(trajectory.size)
-    matrix[2, :] = _DHC_YY_operators(trajectory.size)
-    matrix[3, :] = _DHC_ZZ_operators(trajectory.size)
-    return matrix
+	matrix = Matrix{PauliOperator}(undef, 3, 3 * trajectory.size^2)
+	matrix[1, :] = _DHC_XX_operators(trajectory.size)
+	matrix[2, :] = _DHC_YY_operators(trajectory.size)
+	matrix[3, :] = _DHC_ZZ_operators(trajectory.size)
+	return matrix
 end
 
 function get_operators(trajectory::YaoKivelsonOrientableTrajectory)
-    L = trajectory.size
-    vector = Vector{PauliOperator}(undef, (3+4)*L^2)
-    vector[1:4*L^2] = _DHC_J_operators_orientable(L)
-    vector[4*L^2+1:end] = _DHC_K_operators(L)
-    return vector
+	L = trajectory.size
+	vector = Vector{PauliOperator}(undef, (3 + 4) * L^2)
+	vector[1:4*L^2] = _DHC_J_operators_orientable(L)
+	vector[4*L^2+1:end] = _DHC_K_operators(L)
+	return vector
 end
 
 function get_operators(trajectory::YaoKivelsonNonorientableTrajectory)
-    L = trajectory.size
-    vector = Vector{PauliOperator}(undef, (3+6)*L^2)
-    vector[1:6*L^2] = _DHC_J_operators_nonorientable(L)
-    vector[6*L^2+1:end] = _DHC_K_operators(L)
-    return vector
+	L = trajectory.size
+	vector = Vector{PauliOperator}(undef, (3 + 6) * L^2)
+	vector[1:6*L^2] = _DHC_J_operators_nonorientable(L)
+	vector[6*L^2+1:end] = _DHC_K_operators(L)
+	return vector
 end
 
-function initialise(trajectory::DecoratedHoneycombTrajectory; basis=:Z) :: MixedDestabilizer
-    L = trajectory.size
-    if basis == :Z
-        bilinears = _DHC_ZZ_operators(L)
-    elseif basis == :X
-        bilinears = _DHC_XX_operators(L)
-    elseif basis == :Y
-        bilinears = _DHC_YY_operators(L)
-    end
-    stabs = [bilinears..., _DHC_largeloop_operators(L)..., _DHC_smallloop_operators(L)..., _DHC_wilsonline_operators(L)...]
-    state = MixedDestabilizer(Stabilizer(stabs))
-    if (QuantumClifford.trusted_rank(state) != trajectory.nqubits) && (trajectory.verbosity == :debug)
-        @warn "Initial state is not pure."
-    end
-    return state
+function initialise(trajectory::DecoratedHoneycombTrajectory; basis = :Z)::MixedDestabilizer
+	L = trajectory.size
+	if basis == :Z
+		bilinears = _DHC_ZZ_operators(L)
+	elseif basis == :X
+		bilinears = _DHC_XX_operators(L)
+	elseif basis == :Y
+		bilinears = _DHC_YY_operators(L)
+	end
+	stabs = [bilinears..., _DHC_largeloop_operators(L)..., _DHC_smallloop_operators(L)..., _DHC_wilsonline_operators(L)...]
+	state = MixedDestabilizer(Stabilizer(stabs))
+	if (QuantumClifford.trusted_rank(state) != trajectory.nqubits) && (trajectory.verbosity == :debug)
+		@warn "Initial state is not pure."
+	end
+	return state
 end
 
 # function _DHC_randomXXmeasurement!(state::QuantumClifford.MixedDestabilizer)
@@ -129,88 +129,99 @@ end
 ### Dynamics ###
 
 function circuit!(state::QuantumClifford.MixedDestabilizer, trajectory::YaoKivelsonXYZTrajectory, operators)
-    px = trajectory.params[1]
-    py = trajectory.params[2]
-    pz = trajectory.params[3]
-    L = trajectory.size
-    for subtime in 1:trajectory.nqubits
-        p = rand()
-        if p < px
-            project!(state, operators[1, rand(1:3*L^2)], keep_result=false, phases=false)
-        elseif p < px + py
-            project!(state, operators[2, rand(1:3*L^2)], keep_result=false, phases=false)
-        elseif p < px + py + pz
-            project!(state, operators[3, rand(1:3*L^2)], keep_result=false, phases=false)
-        end
-    end
-    return nothing
+	px = trajectory.params[1]
+	py = trajectory.params[2]
+	pz = trajectory.params[3]
+	L = trajectory.size
+	for subtime in 1:trajectory.nqubits
+		p = rand()
+		if p < px
+			project!(state, operators[1, rand(1:3*L^2)], keep_result = false, phases = false)
+		elseif p < px + py
+			project!(state, operators[2, rand(1:3*L^2)], keep_result = false, phases = false)
+		elseif p < px + py + pz
+			project!(state, operators[3, rand(1:3*L^2)], keep_result = false, phases = false)
+		end
+	end
+	return nothing
 end
 
 function circuit!(state::QuantumClifford.MixedDestabilizer, trajectory::YaoKivelsonOrientableTrajectory, operators)
-    J = trajectory.params[1] # probability of triangular measurement, orientable: silence triangular Z bond, non-orientable: all bonds
-    K = trajectory.params[2] # probability of hexagonal/Kitaev-style measurement
-    for subtime in 1:trajectory.nqubits
-        p = rand()
-        if p < J
-            project!(state, operators[rand(1:4*trajectory.size^2)], keep_result=false, phases=false)
-        else
-            project!(state, operators[4*trajectory.size^2+rand(1:3*trajectory.size^2)], keep_result=false, phases=false)
-        end
+	J = trajectory.params[1] # probability of triangular measurement, orientable: silence triangular Z bond, non-orientable: all bonds
+	K = trajectory.params[2] # probability of hexagonal/Kitaev-style measurement
+	for subtime in 1:trajectory.nqubits
+		p = rand()
+		if p < J
+			project!(state, operators[rand(1:4*trajectory.size^2)], keep_result = false, phases = false)
+		else
+			project!(state, operators[4*trajectory.size^2+rand(1:3*trajectory.size^2)], keep_result = false, phases = false)
+		end
 
-    end
+	end
 end
 
 function circuit!(state::QuantumClifford.MixedDestabilizer, trajectory::YaoKivelsonNonorientableTrajectory, operators)
-    J = trajectory.params[1] # probability of triangular measurement, orientable: silence triangular Z bond, non-orientable: all bonds
-    K = trajectory.params[2] # probability of hexagonal/Kitaev-style measurement
-    for subtime in 1:trajectory.nqubits
-        p = rand()
-        if p < J
-            project!(state, operators[rand(1:6*trajectory.size^2)], keep_result=false, phases=false)
-        else
-            project!(state, operators[6*trajectory.size^2+rand(1:3*trajectory.size^2)], keep_result=false, phases=false)
-        end
+	J = trajectory.params[1] # probability of triangular measurement, orientable: silence triangular Z bond, non-orientable: all bonds
+	K = trajectory.params[2] # probability of hexagonal/Kitaev-style measurement
+	for subtime in 1:trajectory.nqubits
+		p = rand()
+		if p < J
+			project!(state, operators[rand(1:6*trajectory.size^2)], keep_result = false, phases = false)
+		else
+			project!(state, operators[6*trajectory.size^2+rand(1:3*trajectory.size^2)], keep_result = false, phases = false)
+		end
 
-    end
+	end
 end
 
 
 ### Observables ###
 
 function entropy(state::QuantumClifford.MixedDestabilizer, trajectory::DecoratedHoneycombTrajectory)
-    algo=Val(:rref)
-    L = trajectory.size
-    EE = zeros(L+1)
+	algo = Val(:rref)
+	L = trajectory.size
+	EE = zeros(L + 1)
+	for i in 1:L
+		EE[i+1] = entanglement_entropy(state, DHC_subsystem(L, 1:i), algo)
+	end
+	return EE
+end
+
+function entropy_clipped(state::QuantumClifford.MixedDestabilizer, trajectory::DecoratedHoneycombTrajectory)
+	bg = bigram(state; clip = true)
+	L = trajectory.size
+	EE = zeros(L + 1)
     for i in 1:L
-        EE[i+1] = entanglement_entropy(state, DHC_subsystem(L, 1:i), algo)
+        subsystem_range = ((i-1)*6*L+1):(i*6*L)
+        EE[i+1] = length(subsystem_range) - count(r->r[1] in subsystem_range && r[2] in subsystem_range, eachrow(bg))
     end
     return EE
 end
 
 function subsystem_labels(trajectory::DecoratedHoneycombTrajectory)
-    L = trajectory.size
-    subsystems = zeros(L+1)
-    for i in 1:L
-        subsystems[i+1] = i
-    end
-    return subsystems
+	L = trajectory.size
+	subsystems = zeros(L + 1)
+	for i in 1:L
+		subsystems[i+1] = i
+	end
+	return subsystems
 end
 
 function tmi(state::QuantumClifford.MixedDestabilizer, trajectory::DecoratedHoneycombTrajectory)
-    algo=Val(:rref)
-    L = trajectory.size
-    if mod(L, 4) != 0
-        @info "L must be a multiple of 4, but is $(L). Tripartite mutual information is ill-defined."
-    end
-    A = DHC_subsystem(L, 1:Int(L/4))
-    B = DHC_subsystem(L, Int(L/4)+1:Int(L/2))
-    C = DHC_subsystem(L, Int(L/2)+1:Int(3L/4))
-    SA = entanglement_entropy(state, A, algo)
-    SB = entanglement_entropy(state, B, algo)
-    SC = entanglement_entropy(state, C, algo)
-    SAB = entanglement_entropy(state, union(A,B), algo)
-    SBC = entanglement_entropy(state, union(B,C), algo)
-    SAC = entanglement_entropy(state, union(A,C), algo)
-    SABC = entanglement_entropy(state, union(A, B, C), algo)
-    return SA + SB + SC - SAB - SBC - SAC + SABC
+	algo = Val(:rref)
+	L = trajectory.size
+	if mod(L, 4) != 0
+		@info "L must be a multiple of 4, but is $(L). Tripartite mutual information is ill-defined."
+	end
+	A = DHC_subsystem(L, 1:Int(L / 4))
+	B = DHC_subsystem(L, Int(L / 4)+1:Int(L / 2))
+	C = DHC_subsystem(L, Int(L / 2)+1:Int(3L / 4))
+	SA = entanglement_entropy(state, A, algo)
+	SB = entanglement_entropy(state, B, algo)
+	SC = entanglement_entropy(state, C, algo)
+	SAB = entanglement_entropy(state, union(A, B), algo)
+	SBC = entanglement_entropy(state, union(B, C), algo)
+	SAC = entanglement_entropy(state, union(A, C), algo)
+	SABC = entanglement_entropy(state, union(A, B, C), algo)
+	return SA + SB + SC - SAB - SBC - SAC + SABC
 end
--- test/clipped.jl
+++ test/clipped.jl
@@ -1,9 +1,51 @@
 using QMOC
-traj = QMOC.trajectory(:YaoKivelsonOrientable, 24, QMOC._number_of_qubits(:YaoKivelsonOrientable, 24), "test", [1//3, 1//3, 1//3], true, :debug, 1, 3*24, 3, 100)
-state = QMOC.initialise(traj)
-operators = QMOC.get_operators(traj)
-t = 0
-state, t = QMOC.thermalise(state, traj, t, operators)
+using QuantumClifford
 
-QMOC.DHC_subsystem(24, [1])
+function entropy(state::QuantumClifford.MixedDestabilizer, trajectory::DecoratedHoneycombTrajectory)
+	algo = Val(:rref)
+	L = trajectory.size
+	EE = zeros(L + 1)
+	for i in 1:L
+        subsystem_range = 1:(i*6*L)
+		EE[i+1] = entanglement_entropy(state, QMOC.DHC_subsystem(), algo)
+	end
+	return EE
+end
 
+function entropy_clipped(state::QuantumClifford.MixedDestabilizer, trajectory::DecoratedHoneycombTrajectory)
+	bg = bigram(state; clip = true)
+	L = trajectory.size
+	EE = zeros(L + 1)
+    for i in 1:L
+        subsystem_range = 1:(i*6*L)
+        EE[i+1] = length(subsystem_range) - count(r->(r[1] in subsystem_range && r[2] in subsystem_range), eachrow(bg))
+    end
+    return EE
+end
+
+p = 0.5
+trajectory = QMOC.trajectory(
+    :YaoKivelsonNonorientable, 
+    12, 
+    QMOC._number_of_qubits(:YaoKivelsonNonorientable, 2),
+    "YaoKivelsonNonorientableTest",
+    [p,1-p],
+    false,
+    :debug,
+    1,
+    3 * 12,
+    3,
+    1
+)
+
+state, time, existing_measurements = QMOC.hot_start(trajectory)
+
+operators = QMOC.get_operators(trajectory)
+
+state, time = QMOC.thermalise(state, trajectory, time, operators)
+
+using BenchmarkTools
+@benchmark entropy(state, trajectory)
+@benchmark entropy_clipped(state, trajectory)
+@benchmark QMOC.entropy(state, trajectory)
+@benchmark QMOC.tmi(state, trajectory)
