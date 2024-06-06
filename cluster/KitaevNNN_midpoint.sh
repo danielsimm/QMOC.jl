9ddfb885dca1590a33e051f19f27daac10630fa0
Daniel Simm <daniel.simm@outlook.com>
2024-06-06 19:54:09 +0200

    midpoint data @ 32

--- cluster/KitaevNNN_midpoint.sh
+++ cluster/KitaevNNN_midpoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 #SBATCH --exclusive
-#SBATCH --nodes=2
+#SBATCH --nodes=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=192
 #SBATCH -p smp
-#SBATCH -J "QMOC_KitaevNNNedge_crit_32"
-#SBATCH --time=12:00:00
+#SBATCH -J "QMOC_KitaevNNN_midpoint_72"
+#SBATCH --time=24:00:00
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simm@thp.uni-koeln.de
 #SBATCH -A ag-trebst
@@ -13,4 +13,4 @@
 cd QMOC.jl/
 julia --project -e 'using Pkg; pkg"instantiate"'
 julia --project -e 'using Pkg; pkg"precompile"'
-srun -n 2 julia -t192 --project cluster/KitaevNNN_midpoint_32.jl
+srun -n 4 julia -t192 --project cluster/KitaevNNN_midpoint_72.jl
